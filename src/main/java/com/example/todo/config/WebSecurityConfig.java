dadf8fca6743275d310bdceffcfd42f229310f0c
llillilililllill <skenals12@gmail.com>
2023-01-26 11:32:35 +0900

    # user api - 보안 설정

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -2,9 +2,12 @@ package com.example.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -13,5 +16,26 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // 시큐리티 설정
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 시큐리티 빌더
+        http.cors() // 크로스 오리진 정책
+                .and()
+                .csrf() // CSRF 정책
+                .disable()  // 사용 안함 ()
+                .httpBasic().disable()  // 기본 시큐리티 인증 해제. 토큰인증 쓸꺼니까
+                // 세션 기반 인증 안함
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 인증 요청중에서 '/' 경로랑 '/api/auth'로 시작되는 경로는 인증하지 않고 모두 허용
+                .authorizeRequests().antMatchers("/", "/api/auth/**").permitAll()
+                // 그 외의 모든 경로는 인증을 거쳐야함.
+                .anyRequest().authenticated();
+
+        return  http.build();
     }
 }
