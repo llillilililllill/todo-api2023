d80160d2e0362207defa2674e43c9872dd871c12
llillilililllill <skenals12@gmail.com>
2023-01-27 12:15:51 +0900

    # user api - 회원 할일목록 조회 처리

--- src/main/java/com/example/todo/userapi/entity/UserEntity.java
+++ src/main/java/com/example/todo/userapi/entity/UserEntity.java
@@ -7,10 +7,11 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+
 @Setter @Getter @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+@EqualsAndHashCode(of = "id") // id 만 비교해라. 굳이 비번,이름까지 비교할 필요x
 @Builder
 @Entity
 @Table(name = "tbl_user")
@@ -19,17 +20,17 @@ public class UserEntity {
     @Id
     @Column(name = "user_id")
     @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-    private String id;  // 계정명이 아닌 식별코드
+    @GenericGenerator(name ="system-uuid", strategy = "uuid") // uuid : universal unique id (중복되지 않는 id)
+    private String id; //계정명이 아니라 식별 코드
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false) //중복하면 안된다! 중복 제약 조건
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false) //필수다! null 값 x
     private String password;
 
     @Column(nullable = false)
-    private String userName;
+    private  String userName;
 
     @CreationTimestamp
     private LocalDateTime joinDate;
